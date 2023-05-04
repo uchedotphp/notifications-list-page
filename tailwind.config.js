022003108a9d2590207be12c42094ee1734a9c68
nwulu uchechukwu prince <uchenwulu@zuvy.co>
2023-05-04 20:05:17 +0100

    style: extend tailwindcss colors and font-family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      PlusJakartaSans: ["Plus Jakarta Sans", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      colors: {
+        "np-red": "hsl(1, 90%, 64%)",
+        "np-blue": "hsl(219, 85%, 26%)",
+        "np-white": "hsl(0, 0%, 100%)",
+        "very-light-grayish-blue": "hsl(210, 60%, 98%)",
+        "light-grayish-blue-1": "hsl(211, 68%, 94%)",
+        "light-grayish-blue-2": "hsl(205, 33%, 90%)",
+        "grayish-blue": "hsl(219, 14%, 63%)",
+        "dark-grayish-blue": "hsl(219, 12%, 42%)",
+        "very-dark-blue": "hsl(224, 21%, 14%)",
+      },
+    },
   },
   plugins: [],
 };
